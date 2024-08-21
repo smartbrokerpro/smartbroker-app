1f8380e18f29a6c8c10dcc64115be7a4df467bb2
Andrés Becker <andres@smart-broker.pro>
2024-08-21 08:11:52 -0400

    Fix

--- pages/api/projects/filter.js
+++ pages/api/projects/filter.js
@@ -1,10 +1,11 @@
-import { filterProjects } from '@/controllers/projectController';
+// import { filterProjects } from '@/controllers/projectController';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    return filterProjects(req, res);
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  res.status(405).end(`Method ${req.method} Not Allowed`);
+  // if (req.method === 'GET') {
+  //   return filterProjects(req, res);
+  // } else {
+  //   res.setHeader('Allow', ['GET']);
+  //   res.status(405).end(`Method ${req.method} Not Allowed`);
+  // }
 }
