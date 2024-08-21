744dc19529349ccd0a9568a3005e05871a721a4f
Andrés Becker <andres@smart-broker.pro>
2024-08-21 08:16:12 -0400

    Fix

--- pages/api/projects/filter.js
+++ pages/api/projects/filter.js
@@ -1,7 +1,7 @@
 // import { filterProjects } from '@/controllers/projectController';
 
 export default async function handler(req, res) {
-  res.status(405).end(`Method ${req.method} Not Allowed`);
+  res.status(500).json({ error: 'Unable to fetch model fields' });
   // if (req.method === 'GET') {
   //   return filterProjects(req, res);
   // } else {
