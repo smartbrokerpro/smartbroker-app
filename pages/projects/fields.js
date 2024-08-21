c20ca26a034cfa4b4b533e4d99589c57d88fd7a7
Andrés Becker <andres@smart-broker.pro>
2024-08-21 08:09:57 -0400

    Fix

--- pages/projects/fields.js
+++ pages/projects/fields.js
@@ -1,24 +1,26 @@
-import { getProjectModel } from '@/models/projectModel';
-import { getStockModel } from '@/models/stockModel';
+// import { getProjectModel } from '@/models/projectModel';
+// import { getStockModel } from '@/models/stockModel';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const Project = await getProjectModel();
-      const Stock = await getStockModel();
+  res.status(405).end(`Method ${req.method} Not Allowed`);
 
-      const projectFields = Object.keys(Project.schema.paths);
-      const stockFields = Object.keys(Stock.schema.paths);
+  // if (req.method === 'GET') {
+  //   try {
+  //     const Project = await getProjectModel();
+  //     const Stock = await getStockModel();
 
-      const allFields = [...new Set([...projectFields, ...stockFields])];
+  //     const projectFields = Object.keys(Project.schema.paths);
+  //     const stockFields = Object.keys(Stock.schema.paths);
 
-      res.status(200).json(allFields);
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({ error: 'Unable to fetch model fields' });
-    }
-  } else {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  //     const allFields = [...new Set([...projectFields, ...stockFields])];
+
+  //     res.status(200).json(allFields);
+  //   } catch (e) {
+  //     console.error(e);
+  //     res.status(500).json({ error: 'Unable to fetch model fields' });
+  //   }
+  // } else {
+  //   res.setHeader('Allow', ['GET']);
+  //   res.status(405).end(`Method ${req.method} Not Allowed`);
+  // }
 }
